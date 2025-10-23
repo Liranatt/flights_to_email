7a10e53834c5993ea6e0216254c470c8d4841af7
Liran Attar <lirattar@gmail.com>
2025-10-23 11:55:31 +0300

    Update CONFIG.py

--- CONFIG.py
+++ CONFIG.py
@@ -11,7 +11,7 @@ RETURN_DATE = "2026-03-22"
 # Email Config
 # Reads email credentials from the environment variables
 SENDER_EMAIL = os.environ.get('SENDER_EMAIL')
-RECEIVER_EMAIL = "me@yotam.net"
+RECEIVER_EMAIL = "marcelattar@gmail.com"
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
